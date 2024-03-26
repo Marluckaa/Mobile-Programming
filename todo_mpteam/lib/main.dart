836afb0e47c048de5260498b0360bb3acfca74c4
Batbaatar-Anu <104774983+Batbaatar-Anu@users.noreply.github.com>
2024-03-26 15:21:14 +0800

    zaskj vzle

--- todo_mpteam/lib/main.dart
+++ todo_mpteam/lib/main.dart
@@ -1,1 +1,10 @@
 
+import 'package:flutter/material.dart';
+import 'package:todo_mpteam/screens/sign_up.dart';
+
+void main() {
+runApp(MaterialApp(
+debugShowCheckedModeBanner: false,
+home: signUpScreen(),
+));
+}
---
+++ todo_mpteam/lib/screens/sign_up.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class signUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+// TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Create new account",
+              style: TextStyle(
+                backgroundColor: Color.fromARGB(255, 131, 177, 255),
+                fontFamily: "Lato",
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                fontSize: 23,
+              ),
+            ),
+            const Text(
+              "Welcome to our app",
+              style: TextStyle(
+                fontFamily: "Lato",
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.2,
+                fontSize: 18,
+              ),
+            ),
+            Image.network(
+              "https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1200px-Studio_Ghibli_logo.svg.png",
+              height: 200,
+              width: 200,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "Email",
+                style: TextStyle(
+                    fontFamily: "Nunito Sans", fontWeight: FontWeight.w800),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Color.fromARGB(255, 206, 205, 255),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(9),
+                ),
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                labelText: "Enter your Email address",
+                labelStyle: TextStyle(
+                  fontFamily: "NUnito Sans",
+                  fontSize: 14,
+                  letterSpacing: 0.1,
+                ),
+                prefixIcon: Icon(
+                  Icons.email,
+                  size: 20,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "Password",
+                style: TextStyle(
+                    fontFamily: "Nunito Sans", fontWeight: FontWeight.w800),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Color.fromARGB(255, 206, 205, 255),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(9),
+                ),
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                labelText: "Create new Password",
+                labelStyle: TextStyle(
+                  fontFamily: "NUnito Sans",
+                  fontSize: 14,
+                  letterSpacing: 0.1,
+                ),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  size: 20,
+                ),
+                suffixIcon: Icon(Icons.visibility),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            InkWell(
+              onTap: (){
+                // inkwell mouse n zurg oorchilno button drhad 
+              },
+              child: Container(
+                width: 320,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: const LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 175, 137, 237),
+                        Color.fromARGB(255, 219, 125, 125)
+                      ],
+                    )),
+                child: Center(
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already have account?",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                InkWell(
+                  onTap: (){
+                    // end bottom darhad hiigdeh vildel bichgdne
+                    Navigator.push(context, MaterialPageRoute(builder:(context){
+                      return logInScreen();
+                    }
+
+                    ));
+                  },
+                  child: Text(
+                    "Log In",
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
